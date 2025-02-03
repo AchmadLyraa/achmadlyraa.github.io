a54044822f707721ecc06e918cbd207a53c4ab43
AchmadLyraa <ahmadbayhaqi069@gmail.com>
2025-02-03 20:54:44 +0800

    mengoptimalkan App.js dan App.test.js

---
+++ build/precache-manifest.44cff9ab42397718a12dd2c5e5019577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af80256d7bea0a76b0e765f2f0712e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0560fd7c6c7a8d909990",
+    "url": "/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "e1403ab571c3225cab51",
+    "url": "/static/css/main.603194f2.chunk.css"
+  },
+  {
+    "revision": "0560fd7c6c7a8d909990",
+    "url": "/static/js/2.b82de0db.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b82de0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1403ab571c3225cab51",
+    "url": "/static/js/main.98cc5745.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
