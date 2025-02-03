2b7f44f5ef01d3f66d63a40da8d49021b6fdcc2f
Achmad Bayhaqi <ahmadbayhaqi069@gmail.com>
2025-02-03 20:26:45 +0800

    menambahkan react-dom pada App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom"; // Pastikan baris ini ada
+import { render } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<App />, div);
 });
